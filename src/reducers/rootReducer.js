92b28ecf7550dd2dd4e54e466afcfa92d3117c37
Binu Varghese <binsdevapps@gmail.com>
2017-12-01 19:30:48 +0530

    Update rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux';
-import movies from '../modules/movies/movies.reducer';
 import AuthReducer from './AuthReducer.js';
 import AppReducer from './AppReducer.js';
 import LocaleReducer from './LocaleReducer.js';
 
 const rootReducer = combineReducers({
-	movies, auth: AuthReducer, appState: AppReducer, locale: LocaleReducer
+	auth: AuthReducer, appState: AppReducer, locale: LocaleReducer
 });
 
 export default rootReducer;
